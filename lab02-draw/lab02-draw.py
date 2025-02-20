efcde8c1beec69dcf880cc15cb579c43d9f6032e
Sergio <sergio.peiteado@edu.uah.es>
2025-02-20 15:38:57 +0100

    mifotoconfunciones

--- lab02-draw/lab02-draw.py
+++ lab02-draw/lab02-draw.py
@@ -21,23 +21,23 @@ def estrellas():
     arcade.draw_circle_filled(300, 600, 4, arcade.color.ANTI_FLASH_WHITE,0)
     arcade.draw_circle_filled(450, 70, 4, arcade.color.ANTI_FLASH_WHITE,0)
     arcade.draw_circle_filled(700, 300, 4, arcade.color.ANTI_FLASH_WHITE,0)
-def gab():
-    arcade.draw_circle_filled(280, 300, 19, arcade.color.ARSENIC,0)
-    arcade.draw_ellipse_filled(385,284,200,20,arcade.color.ARSENIC,5)
+def gab(center_x:float, center_y:float):
+    arcade.draw_circle_filled(center_x, 300, 19, arcade.color.ARSENIC,0)  #cabesa
+    arcade.draw_ellipse_filled(385,center_y,200,20,arcade.color.ARSENIC,5)  #torso
 
-    arcade.draw_ellipse_filled(515,285,90,10,arcade.color.ARSENIC,-10)
-    arcade.draw_ellipse_filled(480,295,60,10,arcade.color.ARSENIC,-38)
-    arcade.draw_ellipse_filled(518,310,40,10,arcade.color.ARSENIC,7)
+    arcade.draw_ellipse_filled(515,285,90,10,arcade.color.ARSENIC,-10)   #pata_recta
+    arcade.draw_ellipse_filled(480,295,60,10,arcade.color.ARSENIC,-38)   #pata_curva_1
+    arcade.draw_ellipse_filled(518,310,40,10,arcade.color.ARSENIC,7)   #pata_curva_2
 
-    arcade.draw_ellipse_filled(350,295,50,10,arcade.color.ARSENIC,-38)
-    arcade.draw_ellipse_filled(353,317,40,10,arcade.color.ARSENIC,30)
-    arcade.draw_ellipse_filled(330,305,50,10,arcade.color.ARSENIC,-60)
+    arcade.draw_ellipse_filled(350,295,50,10,arcade.color.ARSENIC,-38)  #brazo_curvo_1
+    arcade.draw_ellipse_filled(353,317,40,10,arcade.color.ARSENIC,30)  #brazo_curvo_2
+    arcade.draw_ellipse_filled(330,305,50,10,arcade.color.ARSENIC,-60) #brazo_recto
 
-    arcade.draw_triangle_filled(390,340,384,350,290,300,arcade.color.ARSENIC)
+    arcade.draw_triangle_filled(390,340,384,350,290,300,arcade.color.ARSENIC) #flauta
 
 planeta(400,300,177)
 hamaca(400,260,350,80,180,360)
-gab()
+gab(280,284)
 estrellas()
 
 
